fc3398897824d8627d4ada821e8f81e5a7263a08
MaluLenti <maria.lenti@seteloc.com.br>
2023-04-04 17:08:00 -0300

    feat: complete parte 1 of the couse

---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,22 @@
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+};
+var _data, _quantidade, _valor;
+export class Negociacao {
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+    get volume() {
+        return this.quantidade * this.valor;
+    }
+    get data() {
+        const data = new Date(this._data.getTime());
+        return this._data;
+    }
+}
